4054d7d56ed93dd9240a1de23d1163299964caca
koles111ivan <ivan.kolesia@inbox.ru>
2025-04-14 15:06:01 +0300

    Add project files.

---
+++ OnlineShopWebApp1!/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineShopWebApp1_.Models;
+
+namespace OnlineShopWebApp1_.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public string Index(string name, int age)
+        {
+            return name+" "+age;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
